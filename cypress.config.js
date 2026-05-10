02e149341bdabe8a1449b7721113413d93e7aacd
Hermine Harutyunyan <hermineharutyunyan429@gmail.com>
2026-05-10 10:37:31 +0400

    Add Cypress reporters, update config and exclude generated files from Git

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,16 @@
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
+  reporter: 'mochawesome', // HTML reporter
+  reporterOptions: {
+    reportDir: 'cypress/reports/mochawesome',
+    overwrite: false,              
+    html: true,                    
+    json: true,                    
+    charts: true,                  
+    embeddedScreenshots: true,     
+    saveAllAttempts: true          
+  },
   e2e: {
     baseUrl: 'https://practicesoftwaretesting.com',
   },
